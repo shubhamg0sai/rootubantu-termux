979be5a2fc927db9522f22e8b0fce6837e71c53d
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-21 23:42:11 +0530

    Update ubantu2.0.sh

--- ubantu2.0.sh
+++ ubantu2.0.sh
@@ -1,102 +1,298 @@
-#!/data/data/com.termux/files/usr/bin/bash
-pkg install wget -y 
-folder=ubuntu20-fs
-if [ -d "$folder" ]; then
-	first=1
-	echo "skipping downloading"
-fi
-tarball="ubuntu20-rootfs.tar.gz"
-
-if [ "$first" != 1 ];then
-	if [ ! -f $tarball ]; then
-		echo "Download Rootfs, this may take a while base on your internet speed."
-		case `dpkg --print-architecture` in
-		aarch64)
-			archurl="arm64" ;;
-		arm)
-			archurl="armhf" ;;
-		amd64)
-			archurl="amd64" ;;
-		x86_64)
-			archurl="amd64" ;;	
-		*)
-			echo "unknown architecture"; exit 1 ;;
-		esac
-		wget "https://github.com/AndronixApp/AndronixOrigin/raw/master/Rootfs/Ubuntu20/focal-${archurl}.tar.gz" -O $tarball
-
-fi
-	cur=`pwd`
-	mkdir -p "$folder"
-	cd "$folder"
-	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xf ${cur}/${tarball} --exclude=dev||:
-	cd "$cur"
-fi
-mkdir -p ubuntu20-binds
-bin=start-ubuntu20.sh
-echo "writing launch script"
-cat > $bin <<- EOM
-#!/bin/bash
-cd \$(dirname \$0)
-## unset LD_PRELOAD in case termux-exec is installed
-unset LD_PRELOAD
-command="proot"
-command+=" --link2symlink"
-command+=" -0"
-command+=" -r $folder"
-if [ -n "\$(ls -A ubuntu20-binds)" ]; then
-    for f in ubuntu20-binds/* ;do
-      . \$f
-    done
-fi
-command+=" -b /dev"
-command+=" -b /proc"
-command+=" -b ubuntu20-fs/root:/dev/shm"
-## uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
-## uncomment the following line to mount /sdcard directly to / 
-#command+=" -b /sdcard"
-command+=" -w /root"
-command+=" /usr/bin/env -i"
-command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
-command+=" /bin/bash --login"
-com="\$@"
-if [ -z "\$1" ];then
-    exec \$command
-else
-    \$command -c "\$com"
-fi
-EOM
-
-mkdir -p ubuntu20-fs/var/tmp
-rm -rf ubuntu20-fs/usr/local/bin/*
-
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.profile -O ubuntu20-fs/root/.profile.1
-cat $folder/root/.profile.1 >> $folder/root/.profile && rm -rf $folder/root/.profile.1
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vnc -P ubuntu20-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncpasswd -P ubuntu20-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-stop -P ubuntu20-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-start -P ubuntu20-fs/usr/local/bin
-
-chmod +x ubuntu20-fs/root/.bash_profile
-chmod +x ubuntu20-fs/root/.profile
-chmod +x ubuntu20-fs/usr/local/bin/vnc
-chmod +x ubuntu20-fs/usr/local/bin/vncpasswd
-chmod +x ubuntu20-fs/usr/local/bin/vncserver-start
-chmod +x ubuntu20-fs/usr/local/bin/vncserver-stop
-touch $folder/root/.hushlogin
-echo "127.0.0.1 localhost localhost" > $folder/etc/hosts
-echo "nameserver 1.1.1.1" > $folder/etc/resolv.conf
-chmod +x $folder/etc/resolv.conf
-echo "fixing shebang of $bin"
-termux-fix-shebang $bin
-echo "making $bin executable"
-chmod +x $bin
-echo "removing image for some space"
-rm $tarball
-clear
-echo "You can now launch Ubuntu with the ./${bin} script form next time"
-bash $bin
+#CYBER NAME BLACK-KILLER
+#GITHUB: https://github.com/ShuBhamg0sain
+#WHATAPP NO +919557777030
+ShuBhamg0sain=$(mktemp)
+base64 -d  >${ShuBhamg0sain}<<DIXIE
+IyEvZGF0
+YS9kYXRh
+L2NvbS50
+ZXJtdXgv
+ZmlsZXMv
+dXNyL2Jp
+bi9iYXNo
+CnBrZyBp
+bnN0YWxs
+IHdnZXQg
+LXkgCmZv
+bGRlcj11
+YnVudHUy
+MC1mcwpp
+ZiBbIC1k
+ICIkZm9s
+ZGVyIiBd
+OyB0aGVu
+CglmaXJz
+dD0xCgll
+Y2hvICJz
+a2lwcGlu
+ZyBkb3du
+bG9hZGlu
+ZyIKZmkK
+dGFyYmFs
+bD0idWJ1
+bnR1MjAt
+cm9vdGZz
+LnRhci5n
+eiIKCmlm
+IFsgIiRm
+aXJzdCIg
+IT0gMSBd
+O3RoZW4K
+CWlmIFsg
+ISAtZiAk
+dGFyYmFs
+bCBdOyB0
+aGVuCgkJ
+ZWNobyAi
+RG93bmxv
+YWQgUm9v
+dGZzLCB0
+aGlzIG1h
+eSB0YWtl
+IGEgd2hp
+bGUgYmFz
+ZSBvbiB5
+b3VyIGlu
+dGVybmV0
+IHNwZWVk
+LiIKCQlj
+YXNlIGBk
+cGtnIC0t
+cHJpbnQt
+YXJjaGl0
+ZWN0dXJl
+YCBpbgoJ
+CWFhcmNo
+NjQpCgkJ
+CWFyY2h1
+cmw9ImFy
+bTY0IiA7
+OwoJCWFy
+bSkKCQkJ
+YXJjaHVy
+bD0iYXJt
+aGYiIDs7
+CgkJYW1k
+NjQpCgkJ
+CWFyY2h1
+cmw9ImFt
+ZDY0IiA7
+OwoJCXg4
+Nl82NCkK
+CQkJYXJj
+aHVybD0i
+YW1kNjQi
+IDs7CQoJ
+CSopCgkJ
+CWVjaG8g
+InVua25v
+d24gYXJj
+aGl0ZWN0
+dXJlIjsg
+ZXhpdCAx
+IDs7CgkJ
+ZXNhYwoJ
+CXdnZXQg
+Imh0dHBz
+Oi8vZ2l0
+aHViLmNv
+bS9BbmRy
+b25peEFw
+cC9BbmRy
+b25peE9y
+aWdpbi9y
+YXcvbWFz
+dGVyL1Jv
+b3Rmcy9V
+YnVudHUy
+MC9mb2Nh
+bC0ke2Fy
+Y2h1cmx9
+LnRhci5n
+eiIgLU8g
+JHRhcmJh
+bGwKCmZp
+CgljdXI9
+YHB3ZGAK
+CW1rZGly
+IC1wICIk
+Zm9sZGVy
+IgoJY2Qg
+IiRmb2xk
+ZXIiCgll
+Y2hvICJE
+ZWNvbXBy
+ZXNzaW5n
+IFJvb3Rm
+cywgcGxl
+YXNlIGJl
+IHBhdGll
+bnQuIgoJ
+cHJvb3Qg
+LS1saW5r
+MnN5bWxp
+bmsgdGFy
+IC14ZiAk
+e2N1cn0v
+JHt0YXJi
+YWxsfSAt
+LWV4Y2x1
+ZGU9ZGV2
+fHw6Cglj
+ZCAiJGN1
+ciIKZmkK
+bWtkaXIg
+LXAgdWJ1
+bnR1MjAt
+YmluZHMK
+YmluPXN0
+YXJ0LXVidW50dTIwL
+nNoCmVjaG8gIndyaX
+RpbmcgbGF1bmNoIHN
+jcmlwdCIKY2F0ID4g
+JGJpbiA8PC0gRU9NC
+iMhL2Jpbi9iYXNoCm
+NkIFwkKGRpcm5hbWU
+gXCQwKQojIyB1bnNl
+dCBMRF9QUkVMT0FEI
+GluIGNhc2UgdGVybX
+V4LWV4ZWMgaXMgaW5
+zdGFsbGVkCnVuc2V0
+IExEX1BSRUxPQUQKY
+29tbWFuZD0icHJvb3
+QiCmNvbW1hbmQrPSI
+gLS1saW5rMnN5bWxp
+bmsiCmNvbW1hbmQrP
+SIgLTAiCmNvbW1hbm
+QrPSIgLXIgJGZvbGR
+lciIKaWYgWyAtbiAi
+XCQobHMgLUEgdWJ1b
+nR1MjAtYmluZHMpIi
+BdOyB0aGVuCiAgICB
+mb3IgZiBpbiB1YnVu
+dHUyMC1iaW5kcy8qI
+DtkbwogICAgICAuIF
+wkZgogICAgZG9uZQp
+maQpjb21tYW5kKz0i
+IC1iIC9kZXYiCmNvb
+W1hbmQrPSIgLWIgL3
+Byb2MiCmNvbW1hbmQ
+rPSIgLWIgdWJ1bnR1
+MjAtZnMvcm9vdDovZ
+GV2L3NobSIKIyMgdW
+5jb21tZW50IHRoZSB
+mb2xsb3dpbmcgbGlu
+ZSB0byBoYXZlIGFjY
+2VzcyB0byB0aGUgaG
+9tZSBkaXJlY3Rvcnk
+gb2YgdGVybXV4CiNj
+b21tYW5kKz0iIC1iI
+C9kYXRhL2RhdGEvY2
+9tLnRlcm11eC9maWx
+lcy9ob21lOi9yb290
+IgojIyB1bmNvbW1lb
+nQgdGhlIGZvbGxvd2
+luZyBsaW5lIHRvIG1
+vdW50IC9zZGNhcmQg
+ZGlyZWN0bHkgdG8gL
+yAKI2NvbW1hbmQrPS
+IgLWIgL3NkY2FyZCI
+KY29tbWFuZCs9IiAt
+dyAvcm9vdCIKY29tb
+WFuZCs9IiAvdXNyL2
+Jpbi9lbnYgLWkiCmN
+vbW1hbmQrPSIgSE9N
+RT0vcm9vdCIKY29tb
+WFuZCs9IiBQQVRIPS
+91c3IvbG9jYWwvc2J
+pbjovdXNyL2xvY2Fs
+L2JpbjovYmluOi91c
+3IvYmluOi9zYmluOi
+91c3Ivc2JpbjovdXN
+yL2dhbWVzOi91c3Iv
+bG9jYWwvZ2FtZXMiC
+mNvbW1hbmQrPSIgVE
+VSTT1cJFRFUk0iCmN
+vbW1hbmQrPSIgTEFO
+Rz1DLlVURi04Igpjb
+21tYW5kKz0iIC9iaW4vYmFzaCAt
+LWxvZ2luIgpjb209IlwkQCIKaWY
+gWyAteiAiXCQxIiBdO3RoZW4KIC
+AgIGV4ZWMgXCRjb21tYW5kCmVsc
+2UKICAgIFwkY29tbWFuZCAtYyAi
+XCRjb20iCmZpCkVPTQoKbWtkaXI
+gLXAgdWJ1bnR1MjAtZnMvdmFyL3
+RtcApybSAtcmYgdWJ1bnR1MjAtZ
+nMvdXNyL2xvY2FsL2Jpbi8qCgp3
+Z2V0IC1xIGh0dHBzOi8vcmF3Lmd
+pdGh1YnVzZXJjb250ZW50LmNvbS
+9BbmRyb25peEFwcC9BbmRyb25pe
+E9yaWdpbi9tYXN0ZXIvUm9vdGZz
+L1VidW50dTE5Ly5wcm9maWxlIC1
+PIHVidW50dTIwLWZzL3Jvb3QvLn
+Byb2ZpbGUuMQpjYXQgJGZvbGRlc
+i9yb290Ly5wcm9maWxlLjEgPj4g
+JGZvbGRlci9yb290Ly5wcm9maWx
+lICYmIHJtIC1yZiAkZm9sZGVyL3
+Jvb3QvLnByb2ZpbGUuMQp3Z2V0I
+C1xIGh0dHBzOi8vcmF3LmdpdGh1
+YnVzZXJjb250ZW50LmNvbS9BbmR
+yb25peEFwcC9BbmRyb25peE9yaW
+dpbi9tYXN0ZXIvUm9vdGZzL1Vid
+W50dTE5L3ZuYyAtUCB1YnVudHUy
+MC1mcy91c3IvbG9jYWwvYmluCnd
+nZXQgLXEgaHR0cHM6Ly9yYXcuZ2
+l0aHVidXNlcmNvbnRlbnQuY29tL
+0FuZHJvbml4QXBwL0FuZHJvbml4
+T3JpZ2luL21hc3Rlci9Sb290ZnM
+vVWJ1bnR1MTkvdm5jcGFzc3dkIC
+1QIHVidW50dTIwLWZzL3Vzci9sb
+2NhbC9iaW4Kd2dldCAtcSBodHRw
+czovL3Jhdy5naXRodWJ1c2VyY29
+udGVudC5jb20vQW5kcm9uaXhBcH
+AvQW5kcm9uaXhPcmlnaW4vbWFzd
+GVyL1Jvb3Rmcy9VYnVudHUxOS92
+bmNzZXJ2ZXItc3RvcCAtUCB1YnV
+udHUyMC1mcy91c3IvbG9jYWwvYm
+luCndnZXQgLXEgaHR0cHM6Ly9yY
+XcuZ2l0aHVidXNlcmNvbnRlbnQu
+Y29tL0FuZHJvbml4QXBwL0FuZHJ
+vbml4T3JpZ2luL21hc3Rlci9Sb2
+90ZnMvVWJ1bnR1MTkvdm5jc2Vyd
+mVyLXN0YXJ0IC1QIHVidW50dTIw
+LWZzL3Vzci9sb2NhbC9iaW4KCmNobW9kICt
+4IHVidW50dTIwLWZzL3Jvb3QvLmJhc2hfcH
+JvZmlsZQpjaG1vZCAreCB1YnVudHUyMC1mc
+y9yb290Ly5wcm9maWxlCmNobW9kICt4IHVi
+dW50dTIwLWZzL3Vzci9sb2NhbC9iaW4vdm5
+jCmNobW9kICt4IHVidW50dTIwLWZzL3Vzci
+9sb2NhbC9iaW4vdm5jcGFzc3dkCmNobW9kI
+Ct4IHVidW50dTIwLWZzL3Vzci9sb2NhbC9i
+aW4vdm5jc2VydmVyLXN0YXJ0CmNobW9kICt
+4IHVidW50dTIwLWZzL3Vzci9sb2NhbC9iaW
+4vdm5jc2VydmVyLXN0b3AKdG91Y2ggJGZvb
+GRlci9yb290Ly5odXNobG9naW4KZWNobyAi
+MTI3LjAuMC4xIGxvY2FsaG9zdCBsb2NhbGh
+vc3QiID4gJGZvbGRlci9ldGMvaG9zdHMKZW
+NobyAibmFtZXNlcnZlciAxLjEuMS4xIiA+I
+CRmb2xkZXIvZXRjL3Jlc29sdi5jb25mCmNo
+bW9kICt4ICRmb2xkZXIvZXRjL3Jlc29sdi5
+jb25mCmVjaG8gImZpeGluZyBzaGViYW5nIG
+9mICRiaW4iCnRlcm11eC1maXgtc2hlYmFuZ
+yAkYmluCmVjaG8gIm1ha2luZyAkYmluIGV4
+ZWN1dGFibGUiCmNobW9kICt4ICRiaW4KZWN
+obyAicmVtb3ZpbmcgaW1hZ2UgZm9yIHNvbW
+Ugc3BhY2UiCnJtICR0YXJiYWxsCmNsZWFyC
+mVjaG8gIllvdSBjYW4gbm93IGxhdW5jaCBV
+YnVudHUgd
+2l0aCB0aG
+UgLi8ke2J
+pbn0gc2Ny
+aXB0IGZvc
+m0gbmV4dC
+B0aW1lIgp
+iYXNoICRi
+aW4K
+DIXIE
+source ${ShuBhamg0sain}
+rm -rf ${ShuBhamg0sain}
